cf39b7202bf0e67b797d358599bd7380bfcac64c
Daniel <none>
2024-12-03 09:02:59 +0100

    verification function header

--- src/treshold_ecdsa.rs
+++ src/treshold_ecdsa.rs
@@ -7,6 +7,7 @@ use ot::elgamal::Group;
 use p256::ProjectivePoint;
 
 type PublicKey = ProjectivePoint;
+type Signature = (ZpFieldElement, ProjectivePoint);
 
 pub struct ThresholdECDSA {
     bedoza: Bedoza,
@@ -18,7 +19,6 @@ impl ThresholdECDSA {
             bedoza: Bedoza::new(ot_group, zp_group),
         }
     }
-
 
     //Generate a keypair for a specific user, i.e. ([sk_j], pk_j)
     pub fn gen_keypair(&mut self) -> (ShareName, PublicKey) {
@@ -36,8 +36,12 @@ impl ThresholdECDSA {
     }
 
     //Signing a message M using a preproccesed tuple (k, k^-1, sk_j'), the output is a signature (r,s)
-    pub fn sign(&mut self, k: ShareName, k_inv: ShareName, sk_j: ShareName, message: &str) -> (ZpFieldElement, ProjectivePoint) {
+    pub fn sign(&mut self, k: ShareName, k_inv: ShareName, sk_j: ShareName, message: &str) -> Signature {
         todo!()
     }
+}
 
+//Verifying a signature (r,s) on a message M using a public key pk_j
+pub fn verify_signature(pk: PublicKey, message: &str, signature: Signature) -> bool {
+    todo!()
 }
